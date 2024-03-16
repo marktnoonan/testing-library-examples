21f5351d337f0fb824187fa00d15625dbd56dd68
Mark Noonan <mark@cypress.io>
2024-03-16 12:38:16 -0400

    Initialize verify-unhandled-err

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,19 @@
+const shouldNotExecute = () => {
+  throw new Error('Test Override validation should have failed & it block should not have executed.')
+}
+
+Cypress.on('test:before:run', () => {
+  console.log('test:before:run....throw an error')
+  throw new Error('test:before:run....throw an error')
+})
+
+
+it('does not run', () => {
+  shouldNotExecute()
+})
+
+describe('nested', () => {
+  it('does not run 2', () => {
+    shouldNotExecute()
+  })
+})
